7f21f2b421b230b17ac853cd65f8c0ff5355b061
sadaf <sadaf98x@gmail.com>
2020-05-11 14:20:01 +0430

    plotting the function

--- hillClimbing.py
+++ hillClimbing.py
@@ -1,24 +1,37 @@
 import random
 import math
 import numpy
+import matplotlib.pyplot as plt
 
-x = random.uniform(0.5, 2.5)
-fx = math.sin(10*math.pi*x)/(2*x) + (x-1)**4
-step = 0.03125
-a = numpy.gradient(fx, x)
-a1 = numpy.gradient(fx, x-step)
-a2 = numpy.gradient(fx, x+step)
+def f(x):
+    return math.sin(10*math.pi*x)/(2*x) + (x-1)**4
 
-while ( a1<a or a2<a ):
-    if a1<a:
-        x = x-step
-        a = numpy.gradient(fx, x)
-        a1 = numpy.gradient(fx, x-step)
-        a2 = numpy.gradient(fx, x+step)
-    else:
-        x = x+step
-        a = numpy.gradient(fx, x)
-        a1 = numpy.gradient(fx, x-step)
-        a2 = numpy.gradient(fx, x+step)
+# x = random.uniform(0.5, 2.5)
+# step = 0.03125
+# a = numpy.gradient(f(x), x)
+# a1 = numpy.gradient(f(x), x-step)
+# a2 = numpy.gradient(f(x), x+step)
 
-print(x)
+# print("here1")
+# while ( a1<a or a2<a ):
+#     print("here2")
+#     if a1<a:
+#         x = x-step
+#         a = numpy.gradient(f(x), x)
+#         a1 = numpy.gradient(f(x), x-step)
+#         a2 = numpy.gradient(f(x), x+step)
+#     else:
+#         x = x+step
+#         a = numpy.gradient(f(x), x)
+#         a1 = numpy.gradient(f(x), x-step)
+#         a2 = numpy.gradient(f(x), x+step)
+
+# x_value = x
+# y_value = f(x_value)
+fx = numpy.vectorize(f)
+x = numpy.arange(0.5, 2.5, 1)
+plt.plot(x, fx(x)) 
+plt.xlabel('x') 
+plt.ylabel('fx') 
+plt.show() 
+# print(x)
