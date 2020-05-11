60f398ea1148993a640f8d9eebc71841cb2664bc
sadaf <sadaf98x@gmail.com>
2020-05-11 13:33:10 +0430

    hill climbing #1

--- hillClimbing.py
+++ hillClimbing.py
@@ -2,3 +2,23 @@ import random
 import math
 import numpy
 
+fx = math.sin(10*math.pi*x)/(2*x) + (x-1)**4
+x1 = random.uniform(0.5, 2.5)
+step = 0.03125
+a = numpy.gradient(fx, x1)
+a1 = numpy.gradient(fx, x1-step)
+a2 = numpy.gradient(fx, x1+step)
+
+while ( a1<a or a2<a ):
+    if a1<a:
+        x1 = x1-step
+        a = numpy.gradient(fx, x1)
+        a1 = numpy.gradient(fx, x1-step)
+        a2 = numpy.gradient(fx, x1+step)
+    else:
+        x1 = x1+step
+        a = numpy.gradient(fx, x1)
+        a1 = numpy.gradient(fx, x1-step)
+        a2 = numpy.gradient(fx, x1+step)
+
+print(x1)
