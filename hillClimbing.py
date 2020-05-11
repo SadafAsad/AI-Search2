56714f4d4fd76b9882c321b2c54481024485f757
sadaf <sadaf98x@gmail.com>
2020-05-11 13:38:21 +0430

    bug fixes *x not defined*

--- hillClimbing.py
+++ hillClimbing.py
@@ -2,23 +2,23 @@ import random
 import math
 import numpy
 
+x = random.uniform(0.5, 2.5)
 fx = math.sin(10*math.pi*x)/(2*x) + (x-1)**4
-x1 = random.uniform(0.5, 2.5)
 step = 0.03125
-a = numpy.gradient(fx, x1)
-a1 = numpy.gradient(fx, x1-step)
-a2 = numpy.gradient(fx, x1+step)
+a = numpy.gradient(fx, x)
+a1 = numpy.gradient(fx, x-step)
+a2 = numpy.gradient(fx, x+step)
 
 while ( a1<a or a2<a ):
     if a1<a:
-        x1 = x1-step
-        a = numpy.gradient(fx, x1)
-        a1 = numpy.gradient(fx, x1-step)
-        a2 = numpy.gradient(fx, x1+step)
+        x = x-step
+        a = numpy.gradient(fx, x)
+        a1 = numpy.gradient(fx, x-step)
+        a2 = numpy.gradient(fx, x+step)
     else:
-        x1 = x1+step
-        a = numpy.gradient(fx, x1)
-        a1 = numpy.gradient(fx, x1-step)
-        a2 = numpy.gradient(fx, x1+step)
+        x = x+step
+        a = numpy.gradient(fx, x)
+        a1 = numpy.gradient(fx, x-step)
+        a2 = numpy.gradient(fx, x+step)
 
-print(x1)
+print(x)
