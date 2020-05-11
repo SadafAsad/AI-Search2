860b2d3dffe9b4097ea950fd1836f313934b6bc2
sadaf <sadaf98x@gmail.com>
2020-05-11 14:27:20 +0430

    plotting fixed

--- hillClimbing.py
+++ hillClimbing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def f(x):
     return math.sin(10*math.pi*x)/(2*x) + (x-1)**4
 
-# x = random.uniform(0.5, 2.5)
+x = random.uniform(0.5, 2.5)
 # step = 0.03125
 # a = numpy.gradient(f(x), x)
 # a1 = numpy.gradient(f(x), x-step)
@@ -26,12 +26,13 @@ def f(x):
 #         a1 = numpy.gradient(f(x), x-step)
 #         a2 = numpy.gradient(f(x), x+step)
 
-# x_value = x
-# y_value = f(x_value)
+x_value = x
+y_value = f(x_value)
 fx = numpy.vectorize(f)
-x = numpy.arange(0.5, 2.5, 1)
-plt.plot(x, fx(x)) 
+x = numpy.arange(0.5, 2.5, 0.0078125)
+plt.plot(x, fx(x))
+plt.scatter(x_value, y_value, color= "red", marker= "o", s=20)   
 plt.xlabel('x') 
-plt.ylabel('fx') 
+plt.ylabel('fx')
 plt.show() 
 # print(x)
