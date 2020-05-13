f697bf222ddb977b82c4f99ba0593403818506d6
sadaf <sadaf98x@gmail.com>
2020-05-13 13:09:54 +0430

    simulated annealing #3

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -6,25 +6,8 @@ import matplotlib.pyplot as plt
 def f(x):
     return math.sin(10*math.pi*x)/(2*x) + (x-1)**4
 
-x = random.uniform(0.5, 2.5) 
-step = 0.0078125
-a = numpy.gradient([f(x-step), f(x), f(x+step)], [x-step, x, x+step])
-e = 50
+x = random.uniform(0.5, 2.5)
 
-while not ( a[0]<0 and a[2]>0 ):
-    if ( a[0]>0 and a[2]>0 ):
-        if e<0.007:
-            x = x-step
-            a = numpy.gradient([f(x-step), f(x), f(x+step)], [x-step, x, x+step])
-            while not ( a[0]<0 and a[2]>0 ):
-                x = x-step
-                a = numpy.gradient([f(x-step), f(x), f(x+step)], [x-step, x, x+step])
-            break
-        e = e/2
-    x = x+step
-    a = numpy.gradient([f(x-step), f(x), f(x+step)], [x-step, x, x+step])
-
-print(e)
 x_value = x
 y_value = f(x_value)
 print("x=" + str(x_value) + "  y=" + str(y_value))
