d3ad4932c483630ff87eebebebca01e7f66fdac4
sadaf <sadaf98x@gmail.com>
2020-05-14 21:27:24 +0430

    generic algorithm changed !!

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -77,34 +77,47 @@ def solution(population):
 
 def env(population):
     while solution(population)==[]:
-        newPopulation = list()
+
+        #adadye fitness 4ta chrm ha ro peyda mikone
+        fitness_array = list()
         for chrm in population:
-            newPopulation.append(chrm)
+            fitness_array.append(utility(chrm))
+        
+        #bad tarin ro peyda mikone
+        index = 0
+        index_fitness = 0
+        max_fitness = fitness_array[0]
+        for i in fitness_array:
+            if i>max_fitness:
+                max_fitness = i
+                index_fitness = index
+            index+=1
 
-        for i in range(5):
-            for r in range(5):
-                if i!=r:
-                    gen1, gen2 = crossOver(population[i], population[r])
-                    newPopulation.append( mutation(gen1) )
-                    newPopulation.append( mutation(gen2) )
+        #population jadid bad az selection
+        new_population_after_selection = list()
+        for i in range(4):
+            if i!=index_fitness:
+                new_population_after_selection.append(population[i])
+        random_chrm_index = random.randint(0,3)
+        while random_chrm_index==index_fitness:
+            random_chrm_index = random.randint(0,3)
+        new_population_after_selection.append(population[random_chrm_index])
 
-        chrmFitnesses = list()
-        for chrm in newPopulation:
-            chrmFitnesses.append( utility(chrm) )
+        #cross over va mutate rooye in 4ta
+        gen1, gen2 = crossOver(population[0], population[1])
+        population[0] = mutation(gen1)
+        population[1] = mutation(gen2)
+        gen1, gen2 = crossOver(population[2], population[3])
+        population[2] = mutation(gen1)
+        population[3] = mutation(gen2)
 
-        minfitt = numpy.argpartition(chrmFitnesses, 5)
-        newGeneration = list()
-        for i in range(5):
-            newGeneration.append(newPopulation[minfitt[i]])
-        population = newGeneration
     return solution(population)
 
 firstGeneration = [
     [1,0,1,0,1,0,1,0,1,0],
     [0,1,0,1,0,1,0,1,0,1],
     [1,1,0,0,1,0,0,0,1,1],
-    [0,0,1,1,0,0,1,1,0,1],
-    [1,1,1,0,0,1,0,1,0,0]
+    [0,0,1,1,0,0,1,1,0,1]
 ]
 
 ansSum = env(firstGeneration)
