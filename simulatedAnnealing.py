ce942297808552ff183f39dd913927f9f3eb035d
sadaf <sadaf98x@gmail.com>
2020-05-14 16:09:21 +0430

    simulated annealing checked

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -15,24 +15,30 @@ def probability(cost, new_cost, temp):
     else:
         return numpy.exp(-(new_cost-cost)/temp)
 
-x = random.uniform(0.5, 2.5)
-for cycle in range(1000):
-    fraction = cycle/1000
-    t = temperature(fraction)
-    while True:
-        x_next = random.uniform(0.5, 2.5)
-        if x_next != x:
-            break
-    if probability(f(x), f(x_next), t)>random.uniform(0, 1):
-        x = x_next
+def plot(x):
+    x_value = x
+    y_value = f(x_value)
+    print("x=" + str(x_value) + "  y=" + str(y_value))
+    fx = numpy.vectorize(f)
+    x = numpy.arange(0.5, 2.5, 0.0078125)
+    plt.plot(x, fx(x))
+    plt.scatter(x_value, y_value, color= "red", marker= "o", s=10)   
+    plt.xlabel('x') 
+    plt.ylabel('fx')
+    plt.show() 
 
-x_value = x
-y_value = f(x_value)
-print("x=" + str(x_value) + "  y=" + str(y_value))
-fx = numpy.vectorize(f)
-x = numpy.arange(0.5, 2.5, 0.0078125)
-plt.plot(x, fx(x))
-plt.scatter(x_value, y_value, color= "red", marker= "o", s=10)   
-plt.xlabel('x') 
-plt.ylabel('fx')
-plt.show() 
+def simulatedAnnealing(x):
+    for cycle in range(1000):
+        fraction = cycle/1000
+        t = temperature(fraction)
+        while True:
+            x_next = random.uniform(0.5, 2.5)
+            if x_next != x:
+                break
+        if probability(f(x), f(x_next), t)>random.uniform(0, 1):
+            x = x_next
+    return x
+
+x = random.uniform(0.5, 2.5)
+x = simulatedAnnealing(x)
+plot(x)
